180de8ed608c68ec811894517fcea144c66749a5
Sebastian Wagner <sw2000@posteo.de>
2020-11-08 23:49:53 +0100

    namespace game animal damage, function for made-up data

--- tabelle-stacked-cleanup.R
+++ tabelle-stacked-cleanup.R
@@ -3,58 +3,49 @@
 library(ggplot2)
 
 # raw csv data
-"SaSa eaten data RAW"
-sasa_eaten_data_raw <- read.csv2("sasa_eaten.csv", sep=",", dec=".")
-sr <- sasa_eaten_data_raw
+rawdata <- read.csv2("sasa_eaten.csv", sep=",", dec=".")
 
-"SaSa Eaten Data Synthetic"
-syn_year <- c(rep(2000 , 3) , rep(2021 , 3) , rep(2029 , 3) , rep(2063 , 3) )
-syn_year <- rep(as.integer(syn_year), 4)
-syn_treekind <- rep(c("Buche" , "Fichte" , "BI") , 4)
-syn_treekind <- rep(syn_treekind, 4)
-syn_plot <- as.integer(c(rep(1, 12), rep(2, 12), rep(3,12), rep(4,12)))
-syn_damage <- abs(rnorm(12 , 0 , 15))
-syn_damage <- rep(syn_damage, 4)
-synthetic <- data.frame(
-  Jahr=syn_year,
-  Plot=syn_plot,
-  Baumart=syn_treekind,
-  `Höhe1.verbissen`=syn_damage
-)
-
+build_synthetic_data <- function() {
+  syn_year <- c(rep(2000 , 3) , rep(2021 , 3) , rep(2029 , 3) , rep(2063 , 3) )
+  syn_year <- rep(as.integer(syn_year), 4)
+  syn_treekind <- rep(c("Buche" , "Fichte" , "BI") , 4)
+  syn_treekind <- rep(syn_treekind, 4)
+  syn_plot <- as.integer(c(rep(1, 12), rep(2, 12), rep(3,12), rep(4,12)))
+  syn_damage <- abs(rnorm(12 , 0 , 15))
+  syn_damage <- rep(syn_damage, 4)
+  data.frame(
+    Jahr=syn_year,
+    Plot=syn_plot,
+    Baumart=syn_treekind,
+    `Höhe1.verbissen`=syn_damage
+  )
+}
 # synthetic selection
-"selection"
-sr <- synthetic #uncomment to use made-up data
-str(sr)
+rawdata <- build_synthetic_data() #uncomment to use made-up data
 
 # sas raw data internal labled
-"internal labeling"
-srl <- data.frame(
-  year=sr$Jahr,
-  "plot"=sr$Plot,
-  treekind=sr$Baumart,
-  damage=sr$`Höhe1.verbissen`
-)
-#str(srl)
+as_internal_labels <- function(sr) {
+  data.frame(
+    year=sr$Jahr,
+    "plot"=sr$Plot,
+    treekind=sr$Baumart,
+    tree_count.size1.game_damage=sr$`Höhe1.verbissen`
+  )
+}
+internal_data <- as_internal_labels(rawdata)
 
 # raw typing
-"SaSa eaten data after Typing"
-sasa_eaten_data_typed <- data.frame(
-  year=as.factor(srl$year),
-  "plot"=srl$"plot",
-  treekind=srl$treekind,
-  damage=srl$damage
-)
-st <- sasa_eaten_data_typed
-str(st)
-#st
+# convert input int years to Factors
+internal_data$year <- as.factor(internal_data$year)
+clean_internal_data <- internal_data
+
 ## calculations
-st <- subset(st, plot==2)
+st <- subset(clean_internal_data, plot==2)
 
 ## output preparation
 
 # Stacked
-p <- ggplot(st, aes(x=year, fill=treekind, y=damage)) +
+p <- ggplot(st, aes(x=year, fill=treekind, y=tree_count.size1.game_damage)) +
     geom_bar(position="stack", stat="identity") +
     labs(x = 'Jahr', y = 'Höhe1 verbissen', fill = 'Baumart')
     #geom_col(position="dodge")
