61ed522369a46708b29a3f980045a21b23230ba6
Sebastian Wagner <sw2000@posteo.de>
2020-11-08 21:22:25 +0100

    int column plot

--- tabelle-stacked-cleanup.R
+++ tabelle-stacked-cleanup.R
@@ -7,36 +7,55 @@ library(ggplot2)
 sasa_eaten_data_raw <- read.csv2("sasa_eaten.csv", sep=",", dec=".")
 sr <- sasa_eaten_data_raw
 
-# sas raw data internal labled
-"internal labeling"
-srl <- data.frame(year=sr$Jahr,treekind=sr$Baumart,demage=sr$Fraß)
-#str(srl)
-
 "SaSa Eaten Data Synthetic"
 syn_year <- c(rep(2000 , 3) , rep(2021 , 3) , rep(2029 , 3) , rep(2063 , 3) )
-syn_year <- rep(as.integer(syn_year), 2)
+syn_year <- rep(as.integer(syn_year), 4)
 syn_treekind <- rep(c("Buche" , "Fichte" , "Grenzstein") , 4)
-syn_treekind <- rep(syn_treekind, 2)
+syn_treekind <- rep(syn_treekind, 4)
+syn_plot <- as.integer(c(rep(1, 12), rep(2, 12), rep(3,12), rep(4,12)))
 syn_demage <- abs(rnorm(12 , 0 , 15))
-syn_demage <- rep(syn_demage, 2)
-synthetic <- data.frame(year=syn_year,treekind=syn_treekind,demage=syn_demage)
-#str(synthetic)
+syn_demage <- rep(syn_demage, 4)
+synthetic <- data.frame(
+  Jahr=syn_year,
+  Plot=syn_plot,
+  Baumart=syn_treekind,
+  Fraß=syn_demage
+)
 #sasa_eaten_data_synthetic
 
 # synthetic selection
 "selection"
-srl <- synthetic
-str(srl)
+sr <- synthetic
+str(sr)
+
+# sas raw data internal labled
+"internal labeling"
+srl <- data.frame(
+  year=sr$Jahr,
+  "plot"=sr$Plot,
+  treekind=sr$Baumart,
+  demage=sr$Fraß
+)
+#str(srl)
 
 # raw typing
 "SaSa eaten data after Typing"
-sasa_eaten_data_typed <- data.frame(year=as.factor(srl$year),treekind=srl$treekind,demage=srl$demage)
+sasa_eaten_data_typed <- data.frame(
+  year=as.factor(srl$year),
+  "plot"=srl$"plot",
+  treekind=srl$treekind,
+  demage=srl$demage
+)
 st <- sasa_eaten_data_typed
 str(st)
 #st
 
 # relabel only
-sl <- data.frame(Jahr=st$year, Baumart=st$treekind, Schadenshöhe=st$demage)
+sl <- data.frame(
+  Jahr=st$year,
+  Baumart=st$treekind,
+  Schadenshöhe=st$demage
+)
 #str(sl)
 
 # Stacked
